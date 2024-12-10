fa7e61002aa17d2bb92f2203b32867a15de258d9
Willie Shuford <willshuford@gmail.com>
2024-12-09 22:52:05 -0600

    get route created

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-
+router.get('/', ( req,res )=>{
+    console.log ( '/api/todo GET hit' );
+    const queryString = `SELECT * from todo;`; 
+    pool.query( queryString ).then( ( results )=>{
+        res.send( results.rows );
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 400 );
+    })
+})
 // POST
 
 // PUT
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,36 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
+import axios from 'axios'
 
 function App () {
-  
+  const [todoList, setTodoList ] = useState([]);
+
+  useEffect( () =>{
+    fetchTodoList()
+  }, [] );
+
+  function fetchTodoList(){
+    console.log ( 'in fetchTodoList' );
+    axios.get( '/api/todo' ).then( function( response ){
+      setTodoList( response.data );
+    }).catch( function( err ){
+      console.log( err );
+      alert( 'error getting to do list' );
+    })
+  }
+
+  function toggleMe( id ){
+    console.log( 'in toggleMe:', id );
+  }
+
   return (
     <div>
       <h1>TO DO APP</h1>
+      <p>{ JSON.stringify( todoList )}</p>
+      {
+        todoList.map( (item)=>(
+          <p key={ item.id }>{ item.name } <button onClick={ ()=>{ toggleMe( item.id ) }} >Toggle Complete</button></p>
+        ))
+      }
     </div>
   );
 
